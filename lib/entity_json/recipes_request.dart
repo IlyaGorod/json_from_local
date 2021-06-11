a5dca765d8f8392c3aeefaafd7e27b889632f422
ilyaGorod <ilya.gorod.i@gmail.com>
2021-06-11 14:10:15 +0300

    first commit

---
+++ lib/entity_json/recipes_request.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class RecipesList{
+  List<RecipesEntity> recipes;
+
+  RecipesList({this.recipes});
+
+  factory RecipesList.fromJson(Map<String, dynamic> json){
+
+    var recipesJson = json[''] as List;
+
+    List<RecipesEntity> recipesList = recipesJson.map((i) =>
+        RecipesEntity.fromJson(i)).toList();
+
+    return RecipesList(
+       recipes: recipesList,
+    );
+  }
+}
+
+class RecipesEntity {
+  final String id;
+  final String name;
+  final String picture;
+  final String description;
+
+  RecipesEntity({this.id, this.name, this.picture, this.description});
+
+  factory RecipesEntity.fromJson(Map<String, dynamic> json){
+    return RecipesEntity(
+      id: json['id'].toString(),
+      name: json['name'] as String,
+        picture: json['picture'] as String,
+      description: json['description'] as String
+    );
+  }
+}
+
+Future <RecipesList> getRecipesList() async {
+  const url = 'https://github.com/ababicheva/FlutterInternshipTestTask/blob/main/recipes.json';
+  final response = await http.get(Uri.parse(url));
+
+  if(response.statusCode == 200 ) {
+    return RecipesList.fromJson(json.decode(response.body));
+  }else {
+    throw Exception('Failed to load recipes');
+  }
+}
---
+++ lib/recipes.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'entity_json/recipes_request.dart';
+
+class Recipes extends StatefulWidget{
+
+  @override
+  _RecipesState createState() => _RecipesState();
+}
+
+  class _RecipesState extends State<Recipes>{
+
+  Future<RecipesList> recipesList;
+
+  @override
+  void initState(){
+    super.initState();
+    recipesList = getRecipesList();
+  }
+
+  @override
+    Widget build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text('My Recipes'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search, size: 35.0,),
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: FutureBuilder<RecipesList>(
+        future: recipesList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data.recipes.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                        title: Text('${snapshot.data.recipes[index].name}'),
+                        subtitle: Text(
+                            '${snapshot.data.recipes[index].description}'),
+                        leading: Image.network(
+                            '${snapshot.data.recipes[index].picture}')
+                    ),
+                  );
+                }
+            );
+          } else if (snapshot.hasError){
+            return Text('Error');
+          }
+          return Center(child: CircularProgressIndicator(),);
+        },
+      ),
+    );
+  }
+}
